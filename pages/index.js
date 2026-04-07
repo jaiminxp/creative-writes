36896cc75697b35c20aa0c3992b89dff91013448
Jaimin Parmar <jaimins100@gmail.com>
2026-04-07 10:29:12 +0000

    get posts from firestore

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
+import Message from "@/components/message";
+import {useState} from "react";
+import {db} from "@/utils/firebase";
+import {collection, orderBy, query, onSnapshot} from "firebase/firestore";
+
 export default function Home() {
-    return <div></div>;
+    const [allPosts, setAllPosts] = useState([]);
+
+    const getPosts = async () => {
+        const collectionRef = collection(db, 'posts')
+        const q = query(collectionRef, orderBy('timestamp', 'desc'))
+        const unsubscribe = onSnapshot(q, (docs) => {
+            setAllPosts(snapshot.docs.map((doc) => doc.data()));
+        })
+        return unsubscribe;
+    }
+
+    return <div className="my-12 text-lg font-medium">
+        <h2>See what other people are saying</h2>
+        <Message />
+        <Message />
+    </div>;
 }
