b5a9635522a8936c4f61941cf994727c7f9405e1
Jaimin Parmar <jaimins100@gmail.com>
2026-04-07 09:59:14 +0000

    save post to firestore

--- pages/post.js
+++ pages/post.js
@@ -1,22 +1,38 @@
 import {useState} from "react";
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore'
+import {auth, db} from "@/utils/firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {useRouter} from "next/router";
 
 export default function Post() {
     const [post, setPost] = useState({ description: ""})
+    const [user, loading] = useAuthState(auth)
+    const route = useRouter()
 
     const submitPost = async(e) => {
         e.preventDefault()
+        const collectionRef = collection(db, "posts")
+        await addDoc(collectionRef, {
+            ...post,
+            timestamp: serverTimestamp(),
+            user: user.uid,
+            avatar: user.photoURL,
+            username: user.displayName
+        })
+        setPost({ description: ""})
+        return route.push('/')
     }
 
     return (
         <div className="my-20 p-12 shadow-lg rounded-lg max-w-md mx-auto">
-            <form>
+            <form onSubmit={submitPost}>
                 <h1 className="text-2xl font-bold">Create a new post</h1>
                 <div className="py-2">
                     <h3 className="text-lg font-medium py-2">Description</h3>
                     <textarea value={post.description} onChange={(e) => setPost({ ...post, description: e.target.value})} className="bg-gray-800 h-48 w-full text-white text-sm rounded-lg p-2"></textarea>
                     <p>{post.description.length}/300</p>
                 </div>
-                <button onSubmit={submitPost} className="btn-primary w-full">Submit</button>
+                <button className="btn-primary w-full">Submit</button>
             </form>
         </div>
     )
