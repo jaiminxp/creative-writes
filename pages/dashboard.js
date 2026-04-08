bfe3d4b3638b29ab6d2039c3b1713001d40e2ccb
Jaimin Parmar <jaimins100@gmail.com>
2026-04-08 10:29:43 +0000

    render user's posts on dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,17 +1,37 @@
-import {auth} from "@/utils/firebase";
+import {auth, db} from "@/utils/firebase";
 import {useRouter} from "next/router";
 import {useAuthState} from "react-firebase-hooks/auth";
+import {collection, onSnapshot, where, query} from "firebase/firestore";
+import {useEffect, useState} from "react";
+import Message from "@/components/message";
 
 export default function Dashboard() {
     const route = useRouter()
     const [user, loading] = useAuthState(auth)
+    const [posts, setPosts] = useState([]);
 
-    if(loading) return;
-    if(!user) return route.push("/auth/login")
+    const getData = async () => {
+        if(loading) return;
+        if(!user) return route.push("/auth/login")
+
+        const collectionRef = collection(db, 'posts')
+        const q = query(collectionRef, where("user", "==", user.uid))
+        const unsubscribe = onSnapshot(q, ({docs}) => {
+            setPosts(docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+        })
+        return unsubscribe;
+    }
+
+    useEffect(() => {
+        getData()
+    }, [user, loading])
 
     return (
         <div>
             <h1>Your posts</h1>
+            <div>
+                {posts.map((post) => (<Message {...post} key={post.id} />))}
+            </div>
             <button onClick={() => auth.signOut()} className="btn-primary">Sign out</button>
         </div>
     )
