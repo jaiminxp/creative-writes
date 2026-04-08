336b2e32c50d070de5b52a299f2fc816cacf8a9d
Jaimin Parmar <jaimins100@gmail.com>
2026-04-08 10:58:43 +0000

    add delete and edit post buttons

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,6 +4,8 @@ import {useAuthState} from "react-firebase-hooks/auth";
 import {collection, onSnapshot, where, query} from "firebase/firestore";
 import {useEffect, useState} from "react";
 import Message from "@/components/message";
+import { BsTrash2Fill } from 'react-icons/bs'
+import { AiFillEdit } from "react-icons/ai";
 
 export default function Dashboard() {
     const route = useRouter()
@@ -30,7 +32,12 @@ export default function Dashboard() {
         <div>
             <h1>Your posts</h1>
             <div>
-                {posts.map((post) => (<Message {...post} key={post.id} />))}
+                {posts.map((post) => (<Message {...post} key={post.id}>
+                    <div className="flex gap-4">
+                        <button className="btn btn-danger"><BsTrash2Fill className="text-2xl" /> Delete</button>
+                        <button className="btn btn-secondary"><AiFillEdit className="text-2xl" /> Edit</button>
+                    </div>
+                </Message>))}
             </div>
             <button onClick={() => auth.signOut()} className="btn-primary">Sign out</button>
         </div>
